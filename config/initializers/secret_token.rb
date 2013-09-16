99010c4bb2e46f6cb11963a76297f4aa726905a1
Jamie Wilkinson <jamie@jamiedubs.com>
2013-09-15 21:27:43 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Capitalism::Application.config.secret_key_base = 'c318ab9dd4ffe1c558b7da52740ff9de97d2cded12b8da4f4e17937fde2db522e1c2874e1e205bd66e86765d1b95f1f0ec3c7c1f43a7a90beaa7c77e001182cb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Capitalism::Application.config.session_store :cookie_store, key: '_capitalism_session'
